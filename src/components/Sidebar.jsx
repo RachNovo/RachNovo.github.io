f6efe42b8289d814c4a5f373e1f046b8e0cbcb58
RachNovo <r.novoselac@gmail.com>
2020-02-19 17:24:14 -0500

    set up basic css

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import styles, {main} from './Sidebar.css';
+import styles, {main, title, list, post} from './Sidebar.css';
 
 class Sidebar extends Component {
   constructor(props) {
@@ -8,7 +8,28 @@ class Sidebar extends Component {
   render() {
     return (
       <div className={main}>
-        Here's the sidebar!!
+        <div className='picture'>
+
+        </div>
+        <div className={title}>
+        Rachel Novoselac
+        Software Engineer
+        </div>
+
+        <div className={list}>
+          <p>Home</p>
+          <p>About</p>
+          <p>Skills</p>
+          <p>Applications</p>
+          <p>Work Experience</p>
+          <p>Contact</p>
+        </div>
+        <div className={post}>
+          <p>
+            This website was created by me from scratch using React.js
+          </p>
+        </div>
+
       </div>
     )
   }
