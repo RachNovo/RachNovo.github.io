591db36794cde0ac86b13fd7b0a430c27f5245ea
RachNovo <r.novoselac@gmail.com>
2020-02-23 21:53:38 -0500

    started to edit HR section

--- src/components/sections/details/education/education.js
+++ src/components/sections/details/education/education.js
@@ -1,18 +1,18 @@
 module.exports = [
   {
-    school: 'Hack Reactor',
+    school: 'Hack Reactor, Full Stack Software Engineering Immersive Program',
     date: '2019',
-    text: 'yada yada Seeing how things connect excites me! Whether thats data flowing through an application or the way meaning is conveyed in a language. I pick up new tech quickly and enjoy digging through code to find a tough bug. See the experience section to check out my recent applications!',
+    text: '900+ hours of solo and paired programming with the latest technologies (JavaScript, React.js, Node.js, etc). code reviews. ',
     id: 1
   },
   {
-    school: 'Univeristy of Waterloo',
+    school: 'Univeristy of Waterloo, Recreational Therapy',
     date: '2019',
     text: 'yada yada Seeing how things connect excites me! Whether thats data flowing through an application or the way meaning is conveyed in a language. I pick up new tech quickly and enjoy digging through code to find a tough bug. See the experience section to check out my recent applications!',
     id: 2
   },
   {
-    school: 'University of Toronto',
+    school: 'University of Toronto, Honours Bachelor of Arts',
     date: '2019',
     text: 'yada yada Seeing how things connect excites me! Whether thats data flowing through an application or the way meaning is conveyed in a language. I pick up new tech quickly and enjoy digging through code to find a tough bug. See the experience section to check out my recent applications!',
     id: 3
