840b8065033e1add6afd2eb5edc1111a7fa2c01a
RachNovo <r.novoselac@gmail.com>
2020-02-24 15:43:09 -0500

    edited info

--- src/components/sections/details/applications/applications.js
+++ src/components/sections/details/applications/applications.js
@@ -1,14 +1,18 @@
 module.exports = [
   {
     title: 'Marketplace Lodging App',
+    githubLink: '',
+    deployedApp: '',
+    description: 'Designed and built reviews service UI and back end Designed a microservice to paginate a portion of data, based on user selection Deployed service to AWS EC2 instance Ensured app stability by implementing end-to-end testing using Cypress Coordinated with team to deploy proxy with five services',
     pic: 'https://rachel-portfolio.s3.amazonaws.com/IMG_4462+copy.jpg',
-    techList: 'React | CSS Modules | Node.js/Express | MongoDB/Mongoose | AWS EC2/S3 | Cypress',
-    description: 'Designed and built reviews service UI and back end Designed a microservice to paginate a portion of data, based on user selection Deployed service to AWS EC2 instance Ensured app stability by implementing end-to-end testing using Cypress Coordinated with team to deploy proxy with five services'
+    techList: ['React', 'CSS Modules', 'Node.js/Express', 'MongoDB/Mongoose', 'AWS (EC2/S3)', 'Cypress']
   },
   {
-    title: 'Marketplace Lodging App',
+    title: 'Movie Reviews App',
+    githubLink: '',
+    deployedApp: '',
+    description: 'Designed and built reviews service UI and back end Designed a microservice to paginate a portion of data, based on user selection Deployed service to AWS EC2 instance Ensured app stability by implementing end-to-end testing using Cypress Coordinated with team to deploy proxy with five services',
     pic: 'https://rachel-portfolio.s3.amazonaws.com/IMG_4462+copy.jpg',
-    techList: 'React | CSS Modules | Node.js/Express | MongoDB/Mongoose | AWS EC2/S3 | Cypress',
-    description: 'Designed and built reviews service UI and back end Designed a microservice to paginate a portion of data, based on user selection Deployed service to AWS EC2 instance Ensured app stability by implementing end-to-end testing using Cypress Coordinated with team to deploy proxy with five services'
+    techList: ['Node.js/Express', 'PostgreSQL', 'AWS (EC2/S3)', 'K6', 'Loader.io']
   }
 ]
