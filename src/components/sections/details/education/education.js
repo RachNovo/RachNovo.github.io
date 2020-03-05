fb2c8e821276ddee4510cee4c99a47ded4eebdca
RachNovo <r.novoselac@gmail.com>
2020-03-05 03:03:36 -0500

    completed editing

--- src/components/sections/details/education/education.js
+++ src/components/sections/details/education/education.js
@@ -2,8 +2,8 @@ module.exports = [
   {
     school: 'Hack Reactor, Extended Software Engineering Immersive',
     date: '2019',
-    title: 'Advanced Full Stack Javascript Bootcamp',
-    text: '900+ hours over 18 weeks of solo and paired programming using the latest web development technologies and practices (JavaScript, React.js, Node.js, etc) with the goal of producing autonomous engineers. Worked on two distributed teams using agile methodologies.',
+    title: 'Advanced Full Stack Javascript Bootcamp, 18 weeks',
+    text: '900+ hours of solo and paired programming using the latest web development technologies and practices. Intermediate JavaScript knowledge and a demonstration of sufficient autonomy and competency to keep up with the intense pace were required prior to the start date, determined by a selective interview process and 80 hours of mandatory pre-course work. I completed over 100 hours of self-study in preparation for the interview. The introductory section consisted of two-day, paired sprints strengthening CS fundamentals and skills such as debugging, functional programming, automated testing, and recursion. The junior phase continued the sprints completing projects navigating large code bases using unfamiliar technologies. In the senior phase, I worked with two distributed agile teams completing daily standups, merge requests, code reviews and project documentation. Throughout, I completed daily toy problems and created various full-stack mini-app projects to strengthen my knowledge.',
     id: 1
   },
   {
@@ -17,7 +17,7 @@ module.exports = [
     school: 'University of Toronto, Honours Bachelor of Arts',
     date: '2019',
     title: 'Major in Linguistics, Minors in Anthropology and French',
-    text: 'I was Vice President of the Linguistics League for 2 years. I co-founded the tri-campus, Wug Life Initiative: Studying Linguistics at UofT, bringing our passion of Linguistics and the UofT Linguistics program to highschool students across the GTA through live, interactive workshops. I also organized and led various events (ASL: Signing and Deaf Culture, Game of Thrones: Speak Dothraki, Speak Esperanto!) and MC\'d the Linguistics Alumni Panel. I was given a recognition award for outstanding contribution and demonstrating leadership from the Language Studies Department.',
+    text: 'I was Vice President of the Linguistics League for 2 years,  co-founding the tri-campus, Wug Life Initiative: Studying Linguistics at UofT, which brought our passion of Linguistics and the UofT Linguistics program to highschool students across the GTA through live, interactive workshops. I also organized and led various events (ASL: Signing and Deaf Culture, Game of Thrones: Speak Dothraki, Speak Esperanto!) and MC\'d the Linguistics Alumni Panel. I was given a recognition award for outstanding contribution and demonstrating leadership from the Language Studies Department.',
     id: 3
   }
 ]
