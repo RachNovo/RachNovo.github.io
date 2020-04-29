ffc55de29c84796c41c6c7a6a6a1b9cdae8dbdfa
RachNovo <r.novoselac@gmail.com>
2020-04-29 19:56:11 -0400

    edited experience section

--- src/components/details/experience/experiences.js
+++ src/components/details/experience/experiences.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     title: 'Software Engineer, WW (formerly Weight Watchers)',
     date: '2020 to Present',
-    text: 'WW (formerly Weight Watchers) is a global wellness technology company inspiring millions of people to adopt healthy habits for real life. We do this through engaging digital experiences, face-to-face workshops and sustainable programs that encompass healthy eating, physical activity and positive mindset. By drawing on over five decades of experience and expertise in behavioral science, we build communities in order to deliver wellness for all.',
+    text: 'WW (formerly Weight Watchers) is a global wellness technology company inspiring millions of people to adopt healthy habits for real life. We do this through engaging digital experiences, face-to-face workshops and sustainable programs that encompass healthy eating, physical activity and positive mindset. By drawing on over five decades of experience and expertise in behavioral science, we build communities to deliver wellness for all.',
     link: '',
     symbol: 'laptop',
     id: '34',
@@ -10,7 +10,7 @@ module.exports = [
   {
     title: 'Deck Ambassador, British Swim School',
     date: '2019',
-    text: 'The British Swim School creates a unique experience by focusing on building strong relationships with families and kids, ensuring small class sizes and by using the proven BSS curriculum. I managed all aspects of the pool deck with up to 30 parents and children per session and navigated schedules to determine optimal times for lessons and make-up classes.',
+    text: 'The British Swim School creates a unique experience by focusing on building strong relationships with families and kids, ensuring small class sizes and by using the proven BSS curriculum. I managed all aspects of the pool deck with up to 30 parents and children per session and navigated schedules to determine optimal appointment times.',
     link: '',
     symbol: 'pool',
     id: '35',
@@ -18,15 +18,15 @@ module.exports = [
   {
     title: 'Customer Value Associate, Wal-Mart',
     date: '2018-2019',
-    text: 'I worked as part of a team to ensure that the sales-floor was stocked and organized and the prices and signage were correct at all times. I communicated using professionalism and empathy with customers and ensured a clean and safe working environment.',
+    text: 'I worked as part of a team to ensure that the sales-floor was stocked and organized and the prices and signage were accurate at all times. I communicated using professionalism and empathy with customers and ensured a clean and safe working environment.',
     link: '',
     symbol: 'tag',
     id: '36',
   },
   {
-    title: 'Swim Instructor/Head Lifeguard, Various Companies',
+    title: 'Head Lifeguard/Swim Instructor, Various Companies',
     date: '2013-2018',
-    text: 'I led or was part of teams that guarded up to 100 people in pool and beach settings. As a lifeguard, I consistently maintained professionalism and excellent customer care, proactively anticipating dangerous situations and taking steps to ensure client safety. As a swim instructor, I taught children and adults from basic to advanced levels. I took keen interest in my students\' sucess and created customized lesson plans to suite each student and level.',
+    text: 'I led or was part of teams that guarded up to 100 people in beach and pool settings, including as Waterfront Director at a Lake Ontario beach. As a lifeguard, I consistently maintained professionalism and excellent customer care, proactively anticipating dangerous situations and taking steps to ensure client safety. As a swim instructor, I taught children and adults from basic to advanced levels. I took a keen interest in my students\' sucess and created customized lesson plans to suite each student and level.',
     link: '',
     symbol: 'swimmer',
     id: '37',
