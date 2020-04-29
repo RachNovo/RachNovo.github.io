3a6e874b6f742bbbf8e068d2a7c84a39ae5279f3
RachNovo <r.novoselac@gmail.com>
2020-04-29 19:51:44 -0400

    edited about section

--- src/components/details/About.jsx
+++ src/components/details/About.jsx
@@ -13,13 +13,10 @@ function About() {
       </div>
       <div className={aboutText}>
         <span className={boldText}>
-          Hi, I&apos;m Rachel!
+          Hi! I&apos;m Rachel,
         </span>
         {' '}
-        I&apos;m a deep thinker and creative problem solver. I love looking behind the curtain to see how things work! I&apos;m insatiably curious and detail oriented, which helps me pick up new skills quickly. While I value working hard by myself and getting things done, I believe the best results are achieved through collaboration with others.
-        <br />
-        <br />
-        I&apos;m currently a software engineer at WW (formerly Weight Watchers) in Toronto.
+        a software engineer at WW (formerly Weight Watchers) in Toronto. I&apos;m a deep thinker and creative problem solver. I love looking behind the curtain to see how things work! I&apos;m insatiably curious and detail oriented, which helps me pick up new skills quickly. While I value working hard by myself and getting things done, I believe the best results are achieved through collaboration with others.
         <br />
         <br />
         When I&apos;m not coding, I enjoy playing my cello, exploring nature, cooking or practicing my French! (parce que j&apos;ai besoin de pratiquer &#128579;)
